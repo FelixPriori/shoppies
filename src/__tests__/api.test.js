b3ee7e579012951324562a4b0da8defb126ff013
felix <felixriouxsabourin@gmail.com>
2021-01-14 09:43:34 -0500

    import api url

--- src/__tests__/api.test.js
+++ src/__tests__/api.test.js
@@ -8,56 +8,52 @@ import { setupServer } from 'msw/node';
 import { rest } from 'msw';
 import faker from 'faker';
 import MovieNominationsApp from '../components/MovieNominationsApp';
-
-const { REACT_APP_OMDB } = process.env;
+import { apiUrl } from '../externalMovieApi';
 
 const server = setupServer(
-  rest.get(
-    `https://www.omdbapi.com/?apikey=${REACT_APP_OMDB}&type=movie&s=batman`,
-    async (req, res, ctx) => {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          Search: [
-            {
-              Title: 'Batman Begins',
-              Year: '2005',
-              imdbID: 'tt0372784',
-              Type: 'movie',
-              Poster:
-                'https://m.media-amazon.com/images/M/MV5BOTY4YjI2N2MtYmFlMC00ZjcyLTg3YjEtMDQyM2ZjYzQ5YWFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg',
-            },
-            {
-              Title: 'Batman v Superman: Dawn of Justice',
-              Year: '2016',
-              imdbID: 'tt2975590',
-              Type: 'movie',
-              Poster:
-                'https://m.media-amazon.com/images/M/MV5BYThjYzcyYzItNTVjNy00NDk0LTgwMWQtYjMwNmNlNWJhMzMyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg',
-            },
-            {
-              Title: 'Batman',
-              Year: '1989',
-              imdbID: 'tt0096895',
-              Type: 'movie',
-              Poster:
-                'https://m.media-amazon.com/images/M/MV5BMTYwNjAyODIyMF5BMl5BanBnXkFtZTYwNDMwMDk2._V1_SX300.jpg',
-            },
-            {
-              Title: 'Batman Returns',
-              Year: '1992',
-              imdbID: 'tt0103776',
-              Type: 'movie',
-              Poster:
-                'https://m.media-amazon.com/images/M/MV5BOGZmYzVkMmItM2NiOS00MDI3LWI4ZWQtMTg0YWZkODRkMmViXkEyXkFqcGdeQXVyODY0NzcxNw@@._V1_SX300.jpg',
-            },
-          ],
-          totalResults: '328',
-          Response: 'True',
-        }),
-      );
-    },
-  ),
+  rest.get(`${apiUrl}&s=batman`, async (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        Search: [
+          {
+            Title: 'Batman Begins',
+            Year: '2005',
+            imdbID: 'tt0372784',
+            Type: 'movie',
+            Poster:
+              'https://m.media-amazon.com/images/M/MV5BOTY4YjI2N2MtYmFlMC00ZjcyLTg3YjEtMDQyM2ZjYzQ5YWFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg',
+          },
+          {
+            Title: 'Batman v Superman: Dawn of Justice',
+            Year: '2016',
+            imdbID: 'tt2975590',
+            Type: 'movie',
+            Poster:
+              'https://m.media-amazon.com/images/M/MV5BYThjYzcyYzItNTVjNy00NDk0LTgwMWQtYjMwNmNlNWJhMzMyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg',
+          },
+          {
+            Title: 'Batman',
+            Year: '1989',
+            imdbID: 'tt0096895',
+            Type: 'movie',
+            Poster:
+              'https://m.media-amazon.com/images/M/MV5BMTYwNjAyODIyMF5BMl5BanBnXkFtZTYwNDMwMDk2._V1_SX300.jpg',
+          },
+          {
+            Title: 'Batman Returns',
+            Year: '1992',
+            imdbID: 'tt0103776',
+            Type: 'movie',
+            Poster:
+              'https://m.media-amazon.com/images/M/MV5BOGZmYzVkMmItM2NiOS00MDI3LWI4ZWQtMTg0YWZkODRkMmViXkEyXkFqcGdeQXVyODY0NzcxNw@@._V1_SX300.jpg',
+          },
+        ],
+        totalResults: '328',
+        Response: 'True',
+      }),
+    );
+  }),
 );
 
 beforeAll(() => server.listen());
@@ -86,12 +82,9 @@ test('if "Response: false", the error message is displayed', async () => {
   const Error = faker.lorem.sentence();
 
   server.use(
-    rest.get(
-      `https://www.omdbapi.com/?apikey=${REACT_APP_OMDB}&type=movie&s=a`,
-      async (req, res, ctx) => {
-        return res(ctx.status(200), ctx.json({ Response: 'False', Error }));
-      },
-    ),
+    rest.get(`${apiUrl}&s=a`, async (req, res, ctx) => {
+      return res(ctx.status(200), ctx.json({ Response: 'False', Error }));
+    }),
   );
 
   render(<MovieNominationsApp />);
@@ -113,12 +106,9 @@ test('if "Response: false", the error message is displayed', async () => {
 
 test('if there is an unknown error, the error message is displayed', async () => {
   server.use(
-    rest.get(
-      `https://www.omdbapi.com/?apikey=${REACT_APP_OMDB}&type=movie&s=a`,
-      async (req, res, ctx) => {
-        return res(ctx.status(500));
-      },
-    ),
+    rest.get(`${apiUrl}&s=a`, async (req, res, ctx) => {
+      return res(ctx.status(500));
+    }),
   );
 
   render(<MovieNominationsApp />);
