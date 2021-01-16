d0107ee6690c32e6d752965e6257b32de1a9cd52
felix <felixriouxsabourin@gmail.com>
2021-01-16 12:39:44 -0500

    put storage key in variable for reuse and consistency

--- src/hooks/useLocalNominations.jsx
+++ src/hooks/useLocalNominations.jsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from 'react';
 
 export function useLocalNominations(initialState = []) {
+  const storageKey = 'nominations';
   const [nominations, setNominations] = useState(
-    () => JSON.parse(window.localStorage.getItem('nominated')) || initialState,
+    () => JSON.parse(window.localStorage.getItem(storageKey)) || initialState,
   );
 
   useEffect(() => {
     const jsonNominated = JSON.stringify(nominations);
-    window.localStorage.setItem('nominated', jsonNominated);
+    window.localStorage.setItem(storageKey, jsonNominated);
   }, [nominations]);
 
   return [nominations, setNominations];
